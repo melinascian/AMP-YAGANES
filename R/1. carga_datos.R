13f1d06c08b444db2ba39ade629166837a75a0da
TomasMarina <tomasimarina@gmail.com>
2024-04-09 12:53:43 -0300

    Translate & tidy script

--- R/1. carga_datos.R
+++ R/1. carga_datos.R
@@ -1,46 +1,33 @@
-# ---- CARGA DE DATOS ----
+# ---- LOAD PACKAGES ----
 
-# Load data
-lista_interacciones <- read.csv("Datos/lista_interacciones.csv", header = T)  # 'header = T' indica que la 1era fila corresponde a los nombres de las columnas 
-lista_especies <- read.csv("Datos/lista_especies.csv", header = T)
-
-# Load packages
 library(dplyr)
 library(igraph)
 
 
-## Elimino duplicados segun columna especie trofica
+# ---- LOAD INTERACTIONS DATA ----
+
+lista_interacciones <- read.csv("Datos/lista_interacciones_apr24.csv", header = T)
+
+## Delete duplicates
 lista_interaccionesok <- lista_interacciones %>% 
-  group_by(Prey,Predator) %>% 
-  filter(!duplicated(Prey,Predator))
+  group_by(Prey, Predator) %>% 
+  filter(!duplicated(Prey, Predator))
 
 
-# TIM: hay que eliminar las interacciones de baja resolución (*):
-#library(tidyverse)
-#interacciones_ok <- interacciones1 %>% 
-#filter(!str_detect(Presa, "\\*$")) %>% 
-#filter(!str_detect(Depredador, "\\*$")) %>% 
-#distinct(Presa, Depredador, .keep_all = TRUE)
-
-# ----- OBJETO G  -----
-# PASO 1: Creo tabla con columna presa y predador nada mas.
-# Para crear el objeto g (a partir de la columna nº3, los toma como atributos de la interaccion,
-# para agregar atributos: por ejemplo, cantidad de interacciones por especie).
+# ----- CREATE IGRAPH OBJECT  -----
 
 df <- lista_interaccionesok %>% 
   dplyr::select(Prey, Predator) %>% 
   distinct()
 
-# PASO 2: a partir de df, creo objeto g
-
 g <- graph_from_data_frame(df, directed=TRUE)
 
-# PASO 3: creo red de un solo componente
-gcomp <- decompose(g, mode = "weak")  # me dice que hay 2 componentes
+# Check number of network components
+gcomp <- decompose(g, mode = "weak")
+gok <- gcomp[[1]]
 
-gok<- gcomp[[1]]  # permite seleccionar un item dentro de una lista
-gok  # red con un solo componente, el mas grande
 
-# Save results
+# ----- SAVE RESULTS  -----
+
 save(lista_interaccionesok, gok, df,
-     file = "Datos/datosdepurados.rda")  # generar archivo rda
+     file = "Datos/datosdepurados_apr24.rda")
--- R/2. analisis_datos.R
+++ R/2. analisis_datos.R
@@ -1,9 +1,5 @@
-# ---- ANALISIS DE DATOS ----
+# ---- LOAD PACKAGES ----
 
-# Load data
-load("Datos/datosdepurados.rda")
-
-# Load packages
 library(tidyverse)
 library(igraph)
 library(multiweb)
@@ -12,71 +8,22 @@ library(NetworkExtinction)
 library(network)
 
 
-## ---- PROPIEDADES DE COMPLEJIDAD Y ESTRUCTURA ----
+# ---- LOAD TIDY DATA ----
 
+load("Datos/datosdepurados_apr24.rda")
+
+
+## ---- NETWORK-LEVEL ANALYSES ----
+
+### ---- COMPLEXITY & STRUCTURE ----
 fw_props <- multiweb::calc_topological_indices(gok)
 
-## ---- INDICES DE CENTRALIDAD ----
-# calculo los diferentes indices de centralidad para los vertices de gok
+### ---- DEGREE DISTRIBUTION ----
+upgrade_graph(gok)
+g_net <- as.network(as.matrix(gok))
+dist_fit <- NetworkExtinction::DegreeDistribution(g_net)
 
-### ---- GRADO ----
-### numero de interacciones de cada nodo
-hist(degree(gok,mode="all"))
-grado <- as.data.frame(degree(gok))
-
-# agregar como atributo de gok
-V(gok)$degree.total <- degree(gok,mode="all") 
-V(gok)$degree.in <- degree(gok,mode="in") 
-V(gok)$degree.out <- degree(gok,mode="out")
-
-### Ajuste a la distribución de grado
-upgrade_graph(gok)  # actualiza objeto igraph
-g_net <- as.network(as.matrix(gok))  # convierto gok en red
-dist_fit <- NetworkExtinction::DegreeDistribution(g_net)  # ajusto distribución de grado
-
-# menor AIC, mejor modelo
-# menor BIC, mejor modelo
-#logLik    AIC    BIC model    Normal.Resid family     
-#<dbl>  <dbl>  <dbl> <chr>    <chr>        <chr>      
-#  82.5 -159.  -154.  Exp      No           Exponential
-#  23.9  -41.8  -36.8 Power    No           PowerLaw   
-#  18.6  -31.2  -26.2 LogExp   No           Exponential
-# -37.3   80.5   85.5 LogPower No           PowerLaw   
-
-### ---- CERCANÍA ----
-### mide la proximidad de una especie a todo el resto de especies de la red 
-V(gok)$closeness <- igraph::closeness(gok,mode="all")
-
-### ---- INTERMEDIACIÓN ----
-### describe el número de veces que una especie está entre un par de otras especies 
-# (es decir, cuántos caminos llevan a esa especie)
-V(gok)$betweeness <- igraph::betweenness(gok)
-
-a <- intergraph::asDF(gok)  # creo lista a compuesta por nodos e interacciones del obejto g
-atributos <- as.data.frame(a["vertexes"])  # creo el data frame utilizando solo los vertex de la lista "a"
-indices_centralidad <- atributos %>% 
-  rename (ID=vertexes.intergraph_id, name=vertexes.name, degree.total=vertexes.degree.total, 
-          degree.in=vertexes.degree.in, degree.out=vertexes.degree.out, closeness=vertexes.closeness, 
-          betweeness=vertexes.betweeness)
-
-## ---- INDICE DE ESPECIE CLAVE ----
-ind_centralidad <- indices_centralidad %>% 
-  mutate(ranking_degree = dense_rank(desc(degree.total)),
-         ranking_closeness=dense_rank(desc(closeness)),
-         ranking_betweeness=dense_rank(desc(betweeness))) %>% 
-  mutate(IEC=(ranking_degree + ranking_closeness + ranking_betweeness)/3)
-
-# RANKING:
-# mayor grado, mayor ranking
-# mayor cercania, mayor ranking
-# mayor intermediación, mayor ranking
-
-## ---- PATRÓN MUNDO PEQUEÑO ----
-
-# coeficiente de agrupamiento alto y distancia corta 
-# se debe comparar con redes aleatorias con mismo numero de interacciones y especes, pero eso debo gererarlo
-# para redes aleatorias, uso esta funcion, pongo que redes aleatorias me generan, se hace con 100 redes y se compara la estadisitca
-
+### ---- SMALL-WORLD PATTERN ----
 rnd_g <- lapply(1:100, function (x) {
   e <- sample_gnm(fw_props$Size, fw_props$Links, directed = TRUE) # defino redes aleatorias que voy a generar
   # Check that the ER networks has only one connected component
@@ -88,27 +35,57 @@ rnd_g <- lapply(1:100, function (x) {
 
 sw <- multiweb::calc_swness_zscore(gok, nullDist = rnd_g, weights = NA, ncores = 4)
 datos_sw <- as.data.frame(sw["da"])
-# nulldist = distancia de las redes aleatorias con la mia
 
-## ---- NIVELES TRÓFICOS ----
-# https://www.rdocumentation.org/packages/NetIndices/versions/1.4.4.1/topics/TrophInd
-# Funcion TrophInd: me permite calcular los niveles troficos (tl) y el indice de omnivoria (IO)
 
+## ---- SPECIES-LEVEL ANALYSES ----
+### ---- DEGREE ----
+
+hist(degree(gok,mode="all"))
+grado <- as.data.frame(degree(gok))
+
+# Add node attributes
+V(gok)$degree.total <- degree(gok, mode="all") 
+V(gok)$degree.in <- degree(gok, mode="in") 
+V(gok)$degree.out <- degree(gok, mode="out")
+
+### ---- CLOSENESS ----
+V(gok)$closeness <- igraph::closeness(gok,mode="all")
+
+### ---- BETWEENESS ----
+V(gok)$betweeness <- igraph::betweenness(gok)
+
+a <- intergraph::asDF(gok)  # creo lista a compuesta por nodos e interacciones del obejto g
+atributos <- as.data.frame(a["vertexes"])  # creo el data frame utilizando solo los vertex de la lista "a"
+indices_centralidad <- atributos %>% 
+  rename (ID=vertexes.intergraph_id, name=vertexes.name, degree.total=vertexes.degree.total, 
+          degree.in=vertexes.degree.in, degree.out=vertexes.degree.out, closeness=vertexes.closeness, 
+          betweeness=vertexes.betweeness)
+
+### ---- KEYSTONE INDEX ----
+ind_centralidad <- indices_centralidad %>% 
+  mutate(ranking_degree = dense_rank(desc(degree.total)),
+         ranking_closeness=dense_rank(desc(closeness)),
+         ranking_betweeness=dense_rank(desc(betweeness))) %>% 
+  mutate(IEC=(ranking_degree + ranking_closeness + ranking_betweeness)/3)
+
+### ---- TROPHIC LEVEL ----
 m <- get.adjacency(gok, sparse = FALSE)  # matriz de adyacencia
 tl <- round(TrophInd(m), digits = 3)  # tl es el nivel trofico, y OI es el indice de omnivoria (especies con mayor IO, tienen presas dentro de diferentes niveles troficos)
 niveles_troficos <- TrophInd(m)
 
-## ---- SPECIES-LEVEL DATA ----
+### ---- RELATION TL-INDEX ----
+ind_tl <- sp_level %>% 
+  #dplyr::filter(IEC <= 10) %>% 
+  ggplot(aes(x=TL, y=IEC)) + 
+  geom_point() +
+  geom_smooth(method = "loess") +
+  theme_classic()
+
+
+## ---- ALL SPECIES-LEVEL PROPERTIES ----
 sp_level <- cbind(ind_centralidad, niveles_troficos)
 
-## ---- INDICE-TL ----
-(ind_tl <- sp_level %>% 
-   #dplyr::filter(IEC <= 10) %>% 
-   ggplot(aes(x=TL, y=IEC)) + 
-   geom_point() +
-   geom_smooth(method = "loess") +
-   theme_classic())
 
-# Save results
+## ---- SAVE RESULTS ----
 save(gok, fw_props, indices_centralidad, ind_centralidad, datos_sw, niveles_troficos, sp_level,
-     file="Datos/analisis_datos_26jan24.rda")
+     file="Datos/analisis_datos_apr24.rda")
